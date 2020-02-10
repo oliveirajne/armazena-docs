125b91683238a84d6aca212d2c9ab0c96467276b
oliveirajne <oliveirajne@gmail.com>
2020-02-10 00:56:42 -0300

    Input Validation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,21 +6,33 @@
 
 @section('content')
     
+    @if(count($errors) > 0)
+        <div class="row">
+            <div class="col-md-4 offset-md-4">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach                    
+                </ul>
+            </div>
+        </div>
+    @endif
+
     <div class="row">
         <div class="col-md-6">
         <h3>Sign Up</h3>
             <form action="{{ route('signup') }}" method="POST">
                 <div class="form-group">
                     <label for="email">Your E-mail</label>
-                    <input class="form-control" type="text" name="email" id="email">
+                    <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="text" name="email" id="email" value="{{ Request::old('email') }}">
                 </div>
                 <div class="form-group">
                     <label for="first_name">Your First Name</label>
-                    <input class="form-control" type="text" name="first_name" id="first_name">
+                    <input class="form-control {{ $errors->has('first_name') ? 'is-invalid' : '' }}" type="text" name="first_name" id="first_name" value="{{ Request::old('first_name') }}">
                 </div>
                 <div class="form-group">
                     <label for="password">Your Password</label>
-                    <input class="form-control" type="password" name="password" id="password">
+                    <input class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" type="password" name="password" id="password" value="{{ Request::old('password') }}">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 <input type="hidden" name="_token" value="{{ Session::token() }}">
@@ -31,11 +43,11 @@
             <form action="{{ route('signin') }}" method="POST">
                 <div class="form-group">
                     <label for="email">Your E-mail</label>
-                    <input class="form-control" type="text" name="email" id="email">
+                    <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="text" name="email" id="email" value="{{ Request::old('email') }}">
                 </div>
                 <div class="form-group">
                     <label for="password">Your Password</label>
-                    <input class="form-control" type="password" name="password" id="password">
+                    <input class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" type="password" name="password" id="password" value="{{ Request::old('password') }}">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 <input type="hidden" name="_token" value="{{ Session::token() }}">
